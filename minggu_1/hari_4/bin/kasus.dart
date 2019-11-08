d704def7247f8a791645cce9d6630f699a4d1164
YudaDwi <45373781+YudaDwi@users.noreply.github.com>
2019-11-08 09:24:03 +0700

    nggarap kasus
    masih banyak yang belum selesai  dan ada beberapa kata yang belum dipahami

---
+++ minggu_1/hari_4/bin/kasus.dart
@@ -0,0 +1,11 @@
+class Yuda <Y>{ //membuat kelas nama yuda tapi penggunaan huruf Y itu belum paham
+  Y checkItem; //belum paham arti dari checkItem
+
+  Yuda getItem(){
+    return checkItem; //agak lupa fungsi dari return
+  }
+
+  void setItem (T newitem){ //membuat fungsi set item dan menambahkan newitem
+    this.checkItem = newitem;
+  }
+}
