2ec5b2ba6fd4f2708087f9561bc2a35b1fb8059e
YudaDwi <45373781+YudaDwi@users.noreply.github.com>
2019-11-14 19:34:54 +0700

    ini semua ttg animasi
    hero, animated dkk

---
+++ minggu_2/hari 4/animator/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(FlightExample());
+
+class FlightExample extends StatefulWidget {
+  @override
+  _FlightExampleState createState() => _FlightExampleState();
+}
+
+class _FlightExampleState extends State<FlightExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
